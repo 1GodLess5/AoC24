7fd89ae01a6263af7aaf76be8676fa62faa0324a
1GodLess5 <gacekalbert@seznam.cz>
2024-12-02 19:01:49 +0100

    ~ Refactor changes - moved Input file from day one to comply with rules of AoC

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,7 +5,7 @@ import org.example.day1.DayOne;
 public class Main {
 
     public static void main(String[] args) {
-        DayOne dayOne = new DayOne("tasks-inputs/Day1PuzzleInput.txt");
+        DayOne dayOne = new DayOne("src/main/resources/inputFiles/day1/Day1PuzzleInput.txt");
         // Day 1 task 1
         dayOne.totalDistance();
         // Day 1 task 2
